fb15118a6bddc203b5489ce01026f6b235b0daba
torhen <torhen@gmail.com>
2019-10-18 11:18:57 +0200

    Add files via upload

--- webdev.py
+++ webdev.py
@@ -9,14 +9,18 @@ def root():
 	# create default app if nothing existst
 	if not pathlib.Path('app.html').exists():
 		shutil.copy('templates/default_app.html','app.html')
+	if not pathlib.Path('app.js').exists():
+		shutil.copy('templates/default_app.js','app.js')
 
 	return render_template('index.html')
 	
 @app.route('/left')	
 def left():
 	with open('app.html') as fin:
-		text = fin.read()
-	return render_template('left.html', text=text)
+		txt_html = fin.read()
+	with open('app.js') as fin:
+		txt_js = fin.read()
+	return render_template('left.html', txt_html=txt_html, txt_js=txt_js)
 
 @app.route('/right')	
 def right():
@@ -26,10 +30,15 @@ def right():
 def change():
 	""" update file using data from POST """
 	s = str(datetime.datetime.now())
-	txt = request.form['txt']
+	txt_html = request.form['txt_html']
+	txt_js = request.form['txt_js']
 
 	with open('app.html', 'w') as fout:
-		fout.write(txt)
+		fout.write(txt_html)
+		
+	with open('app.js', 'w') as fout:
+		fout.write(txt_js)
+		
 	return "file updated"
 	
 
